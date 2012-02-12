eed0ace074fd3aa5fef0f0eb492d984c044b0971
Tibotanum <biokinton@gmx.de>
2012-02-12 17:11:35 +0100

    Simplified class

--- src/necromunda/Building.java
+++ src/necromunda/Building.java
@@ -3,26 +3,10 @@ package necromunda;
 import com.jme3.math.Vector3f;
 
 public class Building {
-	private Vector3f position;
-	private float rotationAngle;
 	private String[] identifiers;
 	
-	public Building(float rotationAngle, Vector3f origin, String... identifiers) {
-		this.position = origin;
-		this.rotationAngle = rotationAngle;
+	public Building(String... identifiers) {
 		this.identifiers = identifiers;
-	}
-
-	public Vector3f getOrigin() {
-		return position;
-	}
-
-	public void setOrigin(Vector3f origin) {
-		this.position = origin;
-	}
-
-	public float getRotationAngle() {
-		return rotationAngle;
 	}
 
 	public String[] getIdentifiers() {
