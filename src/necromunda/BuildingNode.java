4e5bf7512712ec630373b5165eea13d8c5400f8a
Tibotanum <biokinton@gmx.de>
2012-02-12 18:54:42 +0100

    Added functionality for cloning ladders

--- src/necromunda/BuildingNode.java
+++ src/necromunda/BuildingNode.java
@@ -1,7 +1,11 @@
 package necromunda;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
 import com.jme3.scene.Node;
 
@@ -19,5 +23,37 @@ public class BuildingNode extends Node {
 
 	public void setLadders(List<Ladder> ladders) {
 		this.ladders = ladders;
+	}
+
+	@Override
+	public Node clone(boolean cloneMaterials) {
+		BuildingNode buildingNode = (BuildingNode)super.clone(cloneMaterials);
+		
+		List<Ladder> visitedLadders = new ArrayList<Ladder>();
+		
+		Set<Ladder> clonedLadders = new HashSet<Ladder>();
+		
+		for (Ladder ladder : ladders) {
+			if (visitedLadders.contains(ladder)) {
+				continue;
+			}
+			
+			Ladder clonedLadder = new Ladder(ladder.getLineNode().getLocalTranslation());
+			Ladder clonedPeerLadder = new Ladder(ladder.getPeer().getLineNode().getLocalTranslation());
+			clonedLadder.setPeer(clonedPeerLadder);
+			clonedPeerLadder.setPeer(clonedLadder);
+			
+			clonedLadders.add(clonedLadder);
+			clonedLadders.add(clonedPeerLadder);
+			
+			buildingNode.attachChild(clonedLadder.getLineNode());
+			buildingNode.attachChild(clonedPeerLadder.getLineNode());
+			
+			visitedLadders.add(clonedPeerLadder);
+		}
+		
+		buildingNode.setLadders(new ArrayList<Ladder>(clonedLadders));
+		
+		return buildingNode;
 	}
 }
